88743df3909ca1f4d1fc3510c56650b5da4c29dc
Paulius Putna <paulius@kodnetas.lt>
2018-04-26 14:20:38 +0300

    Changes to publishing. Should work now.

--- KFLA/Startup.cs
+++ KFLA/Startup.cs
@@ -41,8 +41,9 @@ namespace KFLA
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, KFLADBContext context)
         {
+            context.Database.Migrate();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
