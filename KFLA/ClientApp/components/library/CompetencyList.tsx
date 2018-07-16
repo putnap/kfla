6f4efc31bbd13ab09ef21a99b40a7ac2248d2cbb
Paulius Putna <paulius@kodnetas.lt>
2018-07-16 20:38:38 +0300

    Change competency rendering to sort by columns first.

--- KFLA/ClientApp/components/library/CompetencyList.tsx
+++ KFLA/ClientApp/components/library/CompetencyList.tsx
@@ -1,4 +1,4 @@
-﻿import * as React from 'react';
+﻿import * as React from 'react'; 
 import { render } from 'react-dom';
 import { observer, inject } from 'mobx-react';
 import { Competency } from '../../models/Competency';
@@ -7,22 +7,47 @@ import { CompetencyStore } from '../../stores/CompetencyStore';
 import { Loader } from '../Loader';
 import { FactorList } from '../FactorList';
 
+interface CompetencyListState {
+    rows: Competency[][];
+}
+
 interface CompetencyListProps {
     competencies: Competency[];
     renderCompetency: (competency: Competency) => JSX.Element;
 }
 
 @observer
-export class CompetencyList extends React.Component<CompetencyListProps, {}> {
+export class CompetencyList extends React.Component<CompetencyListProps, CompetencyListState> {
+
+    constructor(props: CompetencyListProps) {
+        super(props);
+
+        const numberOfColumns: number = 4;
+        const itemsPerColumn: number = Math.ceil(props.competencies.length / 4);
+        const rows: Competency[][] = [];
+        props.competencies.map((competency, index) => {
+            const rowIndex = index % itemsPerColumn;
+            if (!rows[rowIndex])
+                rows[rowIndex] = [];
+            rows[rowIndex].push(competency);
+        });
+        this.state = { rows: rows };
+    }
 
     render() {
         return <div className='row card mb-2'>
-            <div className='col card-body row'>
+            <div className='col card-body'>
                 {
-                    this.props.competencies.map(competency => {
-                        return <div className='col-3'>
-                            {this.props.renderCompetency(competency)}
-                        </div>;
+                    this.state.rows.map(row => {
+                        return <div className='row'>
+                            {
+                                row.map(competency => {
+                                    return <div className='col-3'>
+                                        {this.props.renderCompetency(competency)}
+                                    </div>;
+                                })
+                            }
+                            </div>
                     })
                 }
             </div>
