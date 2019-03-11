a43a85e11bfbcc2e2649dd6d39c7f5e74414d3f4
Paulius Putna <putna.paulius@gmail.com>
2019-03-11 21:48:49 +0200

    Responsive home.

--- KFLA/ClientApp/components/Home.tsx
+++ KFLA/ClientApp/components/Home.tsx
@@ -16,31 +16,67 @@ export class Home extends React.Component<HomeProps, {}> {
     }
 
     public render() {
-        return <div className='row' role='navigation'>
+        const width = window.innerWidth;
+        return <div role='navigation'>
             <div className='fixed-top m-3'>
                 <img className='w-25' src={require('../logos/logo-korn-ferry.png')} alt='Korn Ferry' />
             </div>
-            <div className='col-sm-12 col-lg-4 background-lib'>
-                <div className='height-100 d-table mx-auto'>
-                    <div className='d-table-cell align-middle text-center'>
-                        <NavLink to={'/library'} activeClassName='active' className='nav-item nav-link navigation-link font-weight-bold' style={{ fontSize: '300%' }}>{this.props.localizationStore.getString('PageTitles.LIBRARY')}</NavLink>
+            {
+                width > 992 ?
+                    <div className='row'>
+                        <div className='col-4 background-lib p-0'>
+                            <div className='height-100 d-table mx-auto'>
+                                <div className='d-table-cell align-middle text-center'>
+                                    <NavLink to={'/library'} activeClassName='active' className='nav-item nav-link navigation-link font-weight-bold' style={{ fontSize: '300%' }}>{this.props.localizationStore.getString('PageTitles.LIBRARY')}</NavLink>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-4 background-light'>
+                            <div className='height-100 d-table mx-auto'>
+                                <div className='d-table-cell align-middle text-center'>
+                                    <NavLink to={'/competencies'} activeClassName='active' className='nav-item nav-link navigation-link font-weight-bold mx-auto' style={{ fontSize: '300%' }}>{this.props.localizationStore.getString('PageTitles.COMPETENCIES')}</NavLink>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-4 background-dark'>
+                            <div className='height-100 d-table mx-auto'>
+                                <div className='d-table-cell align-middle text-center'>
+                                    <NavLink to={'/questions'} activeClassName='active' className='nav-item nav-link navigation-link font-weight-bold' style={{ fontSize: '300%' }}>{this.props.localizationStore.getString('PageTitles.QUESTIONS')}</NavLink>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div className='col-sm-12 col-lg-4 background-light'>
-                <div className='height-100 d-table mx-auto'>
-                    <div className='d-table-cell align-middle text-center'>
-                        <NavLink to={'/competencies'} activeClassName='active' className='nav-item nav-link navigation-link font-weight-bold mx-auto' style={{ fontSize: '300%' }}>{this.props.localizationStore.getString('PageTitles.COMPETENCIES')}</NavLink>
+                    :
+                    <div className='row height-100'>
+                        <div className='col-12 background-lib'>
+                            <div className='h-100 text-center' style={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center'
+                            }}>
+                                <NavLink to={'/library'} activeClassName='active' className='nav-item nav-link navigation-link font-weight-bold' style={{ fontSize: '300%' }}>{this.props.localizationStore.getString('PageTitles.LIBRARY')}</NavLink>
+                            </div>
+                        </div>
+                        <div className='col-12 background-light'>
+                            <div className='h-100 text-center' style={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center'
+                            }}>
+                                <NavLink to={'/competencies'} activeClassName='active' className='nav-item nav-link navigation-link font-weight-bold mx-auto' style={{ fontSize: '300%' }}>{this.props.localizationStore.getString('PageTitles.COMPETENCIES')}</NavLink>
+                            </div>
+                        </div>
+                        <div className='col-12 background-dark'>
+                            <div className='h-100 text-center' style={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center'
+                            }}>
+                                <NavLink to={'/questions'} activeClassName='active' className='nav-item nav-link navigation-link font-weight-bold' style={{ fontSize: '300%' }}>{this.props.localizationStore.getString('PageTitles.QUESTIONS')}</NavLink>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div className='col-sm-12 col-lg-4 background-dark'>
-                <div className='height-100 d-table mx-auto'>
-                    <div className='d-table-cell align-middle text-center'>
-                        <NavLink to={'/questions'} activeClassName='active' className='nav-item nav-link navigation-link font-weight-bold' style={{ fontSize: '300%' }}>{this.props.localizationStore.getString('PageTitles.QUESTIONS')}</NavLink>
-                    </div>
-                </div>
-            </div>
+            }
             <div className='fixed-bottom text-right m-3 font-weight-bold text-white'>
                 <img src={require('../logos/Logo-White-Contrast.png')} alt='Littlefuse' style={{ width: '10%' }} />
                 <br />
@@ -49,4 +85,3 @@ export class Home extends React.Component<HomeProps, {}> {
         </div>;
     }
 }
-    
