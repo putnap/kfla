29d7f1c4cdcbf3dcccae65205c741f27156b161d
Paulius Putna <paulius@kodnetas.lt>
2019-11-09 14:32:47 +0200

    Rework competencies to hooks

--- KFLA/ClientApp/src/components/competencies/CompetenciesContainer.tsx
+++ KFLA/ClientApp/src/components/competencies/CompetenciesContainer.tsx
@@ -1,15 +1,12 @@
 ﻿import * as React from 'react';
 import * as jQuery from 'jquery';
 import { RouteComponentProps } from 'react-router';
-import { observer, inject } from 'mobx-react';
+import { observer } from 'mobx-react-lite';
 import { DndProvider } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend'
 import NavMenu from '../../components/NavMenu';
 import { Loader } from '../../components/Loader';
-
 import { LanguageParam } from '../../@types/types';
-import { CompetencyStore } from '../../stores/CompetencyStore';
-import { LocalizationStore } from '../../stores/LocalizationStore';
 import { useStore } from '../../stores/hook';
 import { VideoModal } from '../../components/VideoModal';
 import { FloatingButtonProps, FloatingActionButtons } from '../../components/FloatingButtons';
@@ -38,79 +35,50 @@ const EvaluationList = ({ evaluations }) => {
     </div>
 }
 
-interface CompetenciesContainerProps extends RouteComponentProps<LanguageParam> {
-    competencyStore?: CompetencyStore
-    localizationStore?: LocalizationStore;
-}
+export const CompetenciesContainer: React.FC<RouteComponentProps<LanguageParam>> = observer(props => {
 
-@inject("competencyStore", "localizationStore")
-@observer
-export class CompetenciesContainer extends React.Component<CompetenciesContainerProps, {}> {
-    constructor(props: CompetenciesContainerProps) {
-        super(props);
+    const { localizationStore, competencyStore } = useStore(stores => stores);
 
-        this.resetEvaluation = this.resetEvaluation.bind(this);
-        this.submitEvaluation = this.submitEvaluation.bind(this);
-        this.randomEvaluation = this.randomEvaluation.bind(this);
+    const resetEvaluation = () => {
+        if (window.confirm(localizationStore.getString('Evaluation.Reset')))
+            competencyStore.resetEvaluation();
     }
 
-    //async componentDidMount() {
-    //    this.props.localizationStore.setTitle('PageTitles.COMPETENCIES');
-    //    const { competencyStore } = this.props;
-    //    if (!competencyStore.isLoaded) {
-    //        await competencyStore.fetchCompetencies();
-    //        await competencyStore.fetchEvaluations();
-    //    }
-    //}
-
-    resetEvaluation() {
-        if (window.confirm(this.props.localizationStore.getString('Evaluation.Reset')))
-            this.props.competencyStore.resetEvaluation();
+    const submitEvaluation = () => {
+        const { language } = props.match.params;
+        props.history.push(`/${language}/evaluation`);
     }
 
-    submitEvaluation() {
-        const { language } = this.props.match.params;
-        this.props.history.push(`/${language}/evaluation`);
-    }
-
-    randomEvaluation() {
-        this.props.competencyStore.evaluations.forEach(evaluation => {
+    const randomEvaluation = () => {
+        competencyStore.evaluations.forEach(evaluation => {
             while (evaluation.Limit > evaluation.evaluatedCompetences) {
-                const competency = this.props.competencyStore.uneavluatedCompetencies[0];
+                const competency = competencyStore.uneavluatedCompetencies[0];
                 competency.evaluateCompetency(evaluation);
             }
         });
-        this.submitEvaluation();
+        submitEvaluation();
     }
 
-    showInfo() {
-        jQuery('#competenciesVideo').modal();
-    }
+    const floatingButtons: FloatingButtonProps[] = [
+        { label: localizationStore.getString('Buttons.Submit'), icon: "check", onClick: submitEvaluation, disabled: !competencyStore.evaluationReady },
+        //{ label: localizationStore.getString('Buttons.Submit'), icon: "random", onClick: randomEvaluation },
+        { label: localizationStore.getString('Buttons.Reset'), icon: "redo", onClick: resetEvaluation },
+        { label: localizationStore.getString('Buttons.Info'), icon: "info", onClick: () => jQuery('#competenciesVideo').modal() },
+    ]
 
-    public render() {
-        const { competencyStore, localizationStore }= this.props;
-
-        const floatingButtons: FloatingButtonProps[] = [
-            { label: localizationStore.getString('Buttons.Info'), icon: "info", onClick: () => jQuery('#competenciesVideo').modal() },
-            { label: localizationStore.getString('Buttons.Submit'), icon: "check", onClick: this.submitEvaluation, disabled: !competencyStore.evaluationReady },
-            //{ label: localizationStore.getString('Buttons.Submit'), icon: "random", onClick: this.randomEvaluation },
-            { label: localizationStore.getString('Buttons.Reset'), icon: "redo", onClick: this.resetEvaluation },
-        ]
-
-        return <DndProvider backend={HTML5Backend}>
-            <div className='row background-light height-100'>
-                <NavMenu {...this.props} />
-                <div className='contentContainer w-100 mx-2 mx-md-5'>
-                    <EvaluationList evaluations={competencyStore.evaluations} />
-                    {
-                        competencyStore.isLoading ?
-                            <Loader text={localizationStore.getString('Evaluation.Loading')} /> :
-                            <CompetencyList />
-                    }
-                    <FloatingActionButtons floatingButtons={floatingButtons} />
-                    <VideoModal id='competenciesVideo' videoId='2yT0gqnKb-A' />
-                </div>
+    return <DndProvider backend={HTML5Backend}>
+        <div className='row background-light height-100'>
+            <NavMenu {...props} />
+            <div className='contentContainer w-100 mx-2 mx-md-5'>
+                <EvaluationList evaluations={competencyStore.evaluations} />
+                {
+                    competencyStore.isLoading ?
+                        <Loader text={localizationStore.getString('Evaluation.Loading')} /> :
+                        <CompetencyList />
+                }
+                <FloatingActionButtons floatingButtons={floatingButtons} />
+                <VideoModal id='competenciesVideo' videoId='2yT0gqnKb-A' />
             </div>
-        </DndProvider>;
-    }
-}
+        </div>
+    </DndProvider>;
+})
--- KFLA/ClientApp/src/components/competencies/CompetencyItem.tsx
+++ KFLA/ClientApp/src/components/competencies/CompetencyItem.tsx
@@ -1,23 +1,10 @@
 ﻿import * as React from 'react';
-import { Competency } from '../../models/Competency';
-import { observer, inject } from 'mobx-react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useDrag } from "react-dnd";
-import { CompetencyStore } from '../../stores/CompetencyStore';
-import { LocalizationStore } from '../../stores/LocalizationStore';
+import { Competency } from '../../models/Competency';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { printList } from '../skillPrinter';
 import { Evaluation } from '../../models/Evaluation';
-
-interface CompetencyItemProps {
-    competency: Competency;
-    competencyStore?: CompetencyStore;
-    localizationStore?: LocalizationStore;
-}
-
-interface CompetencyItemState {
-    flipped: boolean;
-    cardHeight?: number;
-}
+import { useStore } from '../../stores/hook';
 
 const DragableCompetency: React.FC<{ competency: Competency }> = props => {
     const [{ isDragging }, drag] = useDrag({
@@ -44,88 +31,88 @@ const DragableCompetency: React.FC<{ competency: Competency }> = props => {
         </div>)
 }
 
-@inject("competencyStore", "localizationStore")
-@observer
-export class CompetencyItem extends React.Component<CompetencyItemProps, CompetencyItemState> {
-
-    private cardFront: HTMLDivElement;
-    private cardBack: HTMLDivElement;
-
-    constructor(props: CompetencyItemProps) {
-        super(props);
-
-        this.state = {
-            flipped: false,
-            cardHeight: null
-        };
-    }
-
-    flip() {
-        const currentState = this.state.flipped;
-        const currentHeight = this.state.cardHeight;
-        this.setState({ flipped: !currentState, cardHeight: currentHeight });
-    }
-
-    computeCardClass() {
-        let classes = 'card radius-0 flip';
-        if (this.state.flipped)
-            classes += ' flipped';
-        return classes;
-    }
-
-    componentDidMount() {
-        if (this.cardFront.clientHeight > this.cardBack.clientHeight)
-            this.setState({ flipped: this.state.flipped, cardHeight: this.cardFront.clientHeight });
-        else
-            this.setState({ flipped: this.state.flipped, cardHeight: this.cardBack.clientHeight });
-    }
-
-    render() {
-        const cardClasses = this.computeCardClass();
-        return <DragableCompetency competency={this.props.competency}>
-            <div className={cardClasses} >
-                <div className='face front'>
-                    <div ref={ref => this.cardFront = ref} className='card-body' style={{ minHeight: this.state.cardHeight }}>
-                        <h4 className='card-title pb-1 border-bottom border-dark'>
-                            <span>{this.props.competency.ID}.</span>
-                            <span className='pl-3 color-dark'>{this.props.competency.Name}</span>
-                        </h4>
-                        <div className='mr-3'>
-                            <p className='card-text font-weight-bold'>{this.props.competency.Description}</p>
-                            <p className='card-text font-weight-bold'><FontAwesomeIcon icon='plus-circle' className='color-dark' /><span className='pl-2'>{this.props.localizationStore.getString('Skills.SKILLED')}</span></p>
-                            {printList(this.props.competency.Skilled)}
-                        </div>
-                    </div>
-                    <div className='card-footer' style={{ fontSize: '9px' }}>
-                        {this.props.localizationStore.getString('CompetencyItem.Cards')}
-                    </div>
-                    <button type='button' className='btn position-absolute' style={{ background: 'none', top: '50%', right: '5px' }} onClick={(e) => this.flip()}><FontAwesomeIcon icon='angle-right' className='color-dark' style={{ fontSize: '200%' }} /></button>
-                </div>
-                <div className='face back background-dark'>
-                    <div ref={ref => this.cardBack = ref} className='card-body' style={{ minHeight: this.state.cardHeight }}>
-                        <h4 className='card-title pb-1 border-bottom border-white'>
-                            <span>{this.props.competency.ID}.</span>
-                            <span className='pl-3'>{this.props.competency.Name}</span>
-                        </h4>
-                        <div className='row mr-3'>
-                            <div className='col'>
-                                <p className='card-text font-weight-bold'><FontAwesomeIcon icon='exclamation-circle' /><span className='pl-2'>{this.props.localizationStore.getString('Skills.OVERUSED')}</span></p>
-                                {printList(this.props.competency.OverusedSkill)}
-                            </div>
-                            <div className='col'>
-                                <p className='card-text font-weight-bold'><FontAwesomeIcon icon='minus-circle' /><span className='pl-2'>{this.props.localizationStore.getString('Skills.LESS')}</span></p>
-                                {printList(this.props.competency.LessSkilled)}
-                            </div>
-                        </div>
-                    </div>
-                    <div className='card-footer' style={{ fontSize: '9px' }}>
-                        {this.props.localizationStore.getString('CompetencyItem.Cards')}
-                    </div>
-                    <button type='button' className='btn position-absolute' style={{ background: 'none', top: '50%', right: '5px' }} onClick={(e) => this.flip()}><FontAwesomeIcon icon='angle-left' className='color-dark' style={{ fontSize: '200%', color: '#FFFFFF' }} /></button>
-                </div>
-            </div>
-        </DragableCompetency>
-    }
+const computeCardClass = (flipped) => {
+    let classes = 'card radius-0 flip';
+    if (flipped)
+        classes += ' flipped';
+    return classes;
 }
 
+
+export const CompetencyItem: React.FC<{ competency: Competency }> = props => {
+    const { competency } = props;
+
+    const cardFrontRef = React.createRef<HTMLDivElement>();
+    const cardBackRef = React.createRef<HTMLDivElement>();
+
+    const [flipped, flip] = React.useState(false);
+    const [cardHeight, setHeight] = React.useState(null);
+
+    React.useEffect(() => {
+        if (cardFrontRef.current.clientHeight > cardBackRef.current.clientHeight)
+            setHeight(cardFrontRef.current.clientHeight);
+        else
+            setHeight(cardBackRef.current.clientHeight);
+    }, [cardHeight, setHeight, cardBackRef, cardFrontRef]);
+
+    const cardClasses = computeCardClass(flipped);
+
+    return <DragableCompetency competency={competency}>
+        <div className={cardClasses} >
+            <Front competency={competency} cardHeight={cardHeight} frontRef={cardFrontRef} flip={() => flip(true)} />
+            <Back competency={competency} cardHeight={cardHeight} backRef={cardBackRef} flip={() => flip(false)} />
+        </div>
+    </DragableCompetency>
+}
+
+const Front: React.FC<{ competency, cardHeight, frontRef, flip }> = props => {
+    const localizationStore = useStore(stores => stores.localizationStore);
+    const { competency, cardHeight, frontRef, flip } = props;
+
+    return <div className='face front'>
+        <div className='card-body' style={{ minHeight: cardHeight }} ref={frontRef}>
+            <h4 className='card-title pb-1 border-bottom border-dark'>
+                <span>{competency.ID}.</span>
+                <span className='pl-3 color-dark'>{competency.Name}</span>
+            </h4>
+            <div className='mr-3'>
+                <p className='card-text font-weight-bold'>{competency.Description}</p>
+                <p className='card-text font-weight-bold'><FontAwesomeIcon icon='plus-circle' className='color-dark' /><span className='pl-2'>{localizationStore.getString('Skills.SKILLED')}</span></p>
+                {printList(competency.Skilled)}
+            </div>
+        </div>
+        <div className='card-footer' style={{ fontSize: '9px' }}>
+            {localizationStore.getString('CompetencyItem.Cards')}
+        </div>
+        <button type='button' className='btn position-absolute btn-flip' style={{ background: 'none', top: '50%', right: '5px' }} onClick={() => flip()}><FontAwesomeIcon icon='angle-right' /></button>
+    </div>
+}
+
+const Back: React.FC<{ competency, cardHeight, backRef, flip }> = props => {
+    const localizationStore = useStore(stores => stores.localizationStore);
+    const { competency, cardHeight, backRef, flip } = props;
+
+    return <div className='face back'>
+        <div ref={backRef} className='card-body' style={{ minHeight: cardHeight }}>
+            <h4 className='card-title pb-1 border-bottom border-white'>
+                <span>{competency.ID}.</span>
+                <span className='pl-3'>{competency.Name}</span>
+            </h4>
+            <div className='row mr-3'>
+                <div className='col'>
+                    <p className='card-text font-weight-bold'><FontAwesomeIcon icon='exclamation-circle' /><span className='pl-2'>{localizationStore.getString('Skills.OVERUSED')}</span></p>
+                    {printList(competency.OverusedSkill)}
+                </div>
+                <div className='col'>
+                    <p className='card-text font-weight-bold'><FontAwesomeIcon icon='minus-circle' /><span className='pl-2'>{localizationStore.getString('Skills.LESS')}</span></p>
+                    {printList(competency.LessSkilled)}
+                </div>
+            </div>
+        </div>
+        <div className='card-footer' style={{ fontSize: '9px' }}>
+            {localizationStore.getString('CompetencyItem.Cards')}
+        </div>
+        <button type='button' className='btn position-absolute btn-flip' style={{ background: 'none', top: '50%', right: '5px' }} onClick={() => flip()}><FontAwesomeIcon icon='angle-left' /></button>
+    </div>
+}
 
